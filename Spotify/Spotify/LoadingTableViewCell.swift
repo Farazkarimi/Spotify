de00ba006627a9ec5fe6a3ae9e0a6c39c9622746
Faraz Karimi <faraz.karimi1990@gmail.com>
2019-07-11 18:04:13 +0430

    Views Added

---
+++ Spotify/Spotify/LoadingTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingTableViewCell.swift
+//  spotifytest
+//
+//  Created by Faraz on 7/10/19.
+//  Copyright © 2019 Faraz. All rights reserved.
+//
+
+import UIKit
+
+
+class LoadingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var retryButton: UIButton!
+    
+
+}
---
+++ Spotify/Spotify/TrackRowTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TrackRowTableViewCell.swift
+//  spotifytest
+//
+//  Created by Faraz on 7/10/19.
+//  Copyright © 2019 Faraz. All rights reserved.
+//
+
+import UIKit
+
+class TrackRowTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var trackImage: UIImageView!
+    
+}
